8aae79e4d70416dd3fe3f12bba05cd51b7017252
SuperLamaVictor <64604542+SuperLamaVictor@users.noreply.github.com>
2021-04-16 20:50:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,23 +29,41 @@ for i in things.items():
             a += " цена продажи " + str(i[1][j])
     print(a)
     print()
-statik = {}
-for date, count in zip(df1["Дата"][:3857], df1["Продажи в кг"][:3857]):
-    if date not in statik:
-        statik[date] = count
+
+statik_first = {}
+for date, count, money in zip(df1["Дата"][:3857], df1["Продажи в кг"][:3857], df1["Сумма в ценах продажи"][:3857]):
+    if date not in things:
+        statik_first[date] = [count, money]
     else:
-        statik[date] += count
+        statik_first[date][0] += count      # количество в кг
+        statik_first[date][1] += money         # продажа
 
+statik_second = {}
+for date, count, money in zip(df1["Дата"][3858:], df1["Продажи в кг"][3858:], df1["Сумма в ценах продажи"][3858:]):
+    if date not in statik_second:
+        if date not in things:
+            statik_second[date] = [count, money]
+        else:
+            statik_second[date][0] += count  # количество в кг
+            statik_second[date][1] += money  # продажа
+
+date_1, summa_kg_1, summa_1 = [], [], []
+date_2, summa_kg_2, summa_2 = [], [], []
+print(list(statik_first.values()))
+for i in sorted(zip(statik_first.keys(), list(statik_first.values())), key=lambda x: (x[0].split('.')[1], x[0].split('.')[0])):
+    date_1.append(i[0])
+    summa_kg_1.append(i[1][0])
+    summa_1.append(i[1][1])
+print(date_1)
+
+for i in sorted(zip(statik_second.keys(), list(statik_second.values())), key=lambda x: (x[0].split('.')[1], x[0].split('.')[0])):
+    date_1.append(i[0])
+    summa_kg_1.append(i[1][0])
+    summa_1.append(i[1][1])
+
+print("Рост продажи товаров (в кг) составил: " + str(int(sum(summa_2) / sum(summa_1) * 100)) + "%")
+'''
 plt.style.use('dark_background')  # чёерный фон
-plt.plot(statik.keys(), statik.values())
+plt.plot(date, summa)
 plt.show()
-
-for date, count in zip(df1["Дата"][3857:], df1["Продажи в кг"][3857:]):
-    if date not in statik:
-        statik[date] = count
-    else:
-        statik[date] += count
-
-plt.style.use('dark_background')  # чёерный фон
-plt.plot(statik.keys(), statik.values())
-plt.show()
+'''
